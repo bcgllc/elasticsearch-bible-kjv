42063fe367ab5c98a6cec42c1d0c98ad00d08712
Jonathan Burris <jonathanburris@Jonathans-MacBook-Pro-2.local>
2018-06-06 15:35:00 -0400

    update kjv.py

--- kjv.py
+++ kjv.py
@@ -7,6 +7,7 @@
 ###
 
 import os
+import sys
 import errno
 #import datetime, time, calendar
 import json
@@ -15,83 +16,23 @@ import hashlib
 # Third Party Packages
 from elasticsearch import Elasticsearch
 
+if len(sys.argv) == 0:
+	print 'Missing parameter ui:port for elasticsearch'
+	exit
+
 # Set ES Parameters
 index_name = 'bible'
 doc_type_name = 'kjv'
-es = Elasticsearch('<server IP or hostname>')
+es = Elasticsearch(sys.argv[0])
 
 # Import CSV
 import csv
 import sys
 
-files = [
-	'kjv-genesis.csv',
-	'kjv-exodus.csv',
-	'kjv-leviticus.csv',
-	'kjv-numbers.csv',
-	'kjv-deuteronomy.csv',
-	'kjv-joshua.csv',
-	'kjv-judges.csv',
-	'kjv-ruth.csv',
-	'kjv-1samuel.csv',
-	'kjv-2samuel.csv',
-	'kjv-1kings.csv',
-	'kjv-2kings.csv',
-	'kjv-1chronicles.csv',
-	'kjv-2chronicles.csv',
-	'kjv-ezra.csv',
-	'kjv-nehemia.csv',
-	'kjv-esther.csv',
-	'kjv-job.csv',
-	'kjv-psalms.csv',
-	'kjv-proverbs.csv',
-	'kjv-ecclesiastes.csv',
-	'kjv-songofsolomon.csv',
-	'kjv-isaiah.csv',
-	'kjv-jeremiah.csv',
-	'kjv-lamentations.csv',
-	'kjv-ezekiel.csv',
-	'kjv-daniel.csv',
-	'kjv-hosea.csv',
-	'kjv-joel.csv',
-	'kjv-amos.csv',
-	'kjv-obadiah.csv',
-	'kjv-jonah.csv',
-	'kjv-micah.csv',
-	'kjv-nahum.csv',
-	'kjv-habakkuk.csv',
-	'kjv-zephaniah.csv',
-	'kjv-haggai.csv',
-	'kjv-zechariah.csv',
-	'kjv-malachi.csv',
-	'kjv-matthew.csv',
-	'kjv-mark.csv',
-	'kjv-luke.csv',
-	'kjv-john.csv',
-	'kjv-acts.csv',
-	'kjv-romans.csv',
-	'kjv-1corinthians.csv',
-	'kjv-2corinthians.csv',
-	'kjv-galatians.csv',
-	'kjv-ephesians.csv',
-	'kjv-philippians.csv',
-	'kjv-colossians.csv',
-	'kjv-1thessalonians.csv',
-	'kjv-2thessalonians.csv',
-	'kjv-1timothy.csv',
-	'kjv-2timothy.csv',
-	'kjv-titus.csv',
-	'kjv-philemon.csv',
-	'kjv-hebrews.csv',
-	'kjv-james.csv',
-	'kjv-1peter.csv',
-	'kjv-2peter.csv',
-	'kjv-1john.csv',
-	'kjv-2john.csv',
-	'kjv-3john.csv',
-	'kjv-jude.csv',
-	'kjv-revelation.csv'
-]
+files = []
+for filename in os.listdir('csv'):
+    if filename.endswith(".csv"):
+        files.append(filename)
 
 print
 print "Importing CSV files into Elasticsearch Index: \'bible\'"
@@ -121,7 +62,7 @@ for file in files:
 			}
 
 			res = es.index(index=index_name, doc_type=doc_type_name, body=json_es)
-			if res['created'] == False:
+			if res['result'] == False:
 				print
 				print "!!!!!!!!!!!!!!!!!!!!!!!"
 				print "!!! ES ENTRY FAILED !!!"
